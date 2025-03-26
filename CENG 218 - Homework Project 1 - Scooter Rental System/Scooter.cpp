2556819481ab3b68028a6a38ea46eda475136752
Oğuz Kağan <45708619+oguzkaganbilici@users.noreply.github.com>
2025-03-27 00:33:35 +0300

    Delete CENG 218 - Homework Project 1 - Scooter Rental System directory

--- CENG 218 - Homework Project 1 - Scooter Rental System/Scooter.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by Oğuz Kağan Bilici on 23.03.2025.
-//
-
-#include "Scooter.h"
-
-Scooter::Scooter(int id, string loc, int stats, int battery, int distance, string data): next(nullptr), id(id), location(loc), status(stats), battery_level(battery), distance(distance), last_usage_date(data) {};
